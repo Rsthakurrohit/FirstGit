fba8f370faa65b2f4c426d769815311b7a39394a
Rshtakurrohit <rsthakurrohit@gmail.com>
2023-05-28 02:08:40 +0530

    Task5 getElementByTagName

---
+++ Task5.js
@@ -0,0 +1,21 @@
+//getElementByClassName
+
+// let items = document.getElementsByClassName('group-item');
+// items[0].style.color = 'red';
+// items[0].style.fontWeight = 'bold';
+// items[0].style.border = '1px solid black';
+// items[0].style.margin = '10px';
+// items[0].style.padding = '10px';
+// items[0].style.width = '100%';
+
+
+
+// getElementByTagName 
+let items = document.getElementsByTagName('li');
+console.log(items);
+items[4].style.color = 'red';
+items[4].style.fontWeight = 'bold';
+items[4].style.border = '1px solid black';
+items[4].style.margin = '10px';
+items[4].style.padding = '10px';
+items[4].style.width = '100%';
